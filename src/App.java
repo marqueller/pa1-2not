f6ac97ec6431dc043809b2af6f3f6c37a6776fb4
gabrielcedup <600022188@estudante.sed.sc.gov.br>
2024-05-15 20:03:16 -0300

    1º commit

--- src/App.java
+++ src/App.java
@@ -1,20 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // delclaração de variaveis
         String sMsg1 = "Técnico Informática";
         String sMsg2 = "Semestre";
         int iValor1 = 2; // %d
         int iValor2 = 1; // %d
         float fValor1 = 58.700001f; // %f
         char cValor1 = '%'; // %c
-        boolean bExcelencia = true; // %b
+        boolean bExcelência = true; // %b
 
-        // Saída de dados para usuário:
-        System.out.print(sMsg1 + " ");
-        System.out.println("Seja bem vindo!");
-        System.out.println(sMsg1 + "" + " esta no " + sMsg2 + " " + iValor1 + " com " + fValor1 + cValor1 +
-                " do cronograma.");
-        System.out.format("%s esta no %s %d com %f %c do cronograma.", sMsg1, sMsg2, iValor1,
-                fValor1, cValor1);
+        String sMsg3 = "Duvidas???";
+        String sMsg4 = "Por favor chamem!!";
+        String sMsg5 = "Muito obrigado pela atenção";
+        String sMsg6 = String.format("%s \n %s.\n \n %s", sMsg3, sMsg4, sMsg5);
+
+        // Saída de dados para o usuário:
+        System.out.println(sMsg6);
     }
 }
