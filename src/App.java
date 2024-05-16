c17e181f3acacd6a67387faa0261f279f0bc7c99
Ernanicedup <167590964+Ernanicedup@users.noreply.github.com>
2024-05-15 21:04:21 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,18 +1,19 @@
 public class App {
-    public static void main(String[] args) {
-        // Declaração de variáveis:
-        String sMsg1 = "Tecnico Informatica";
+    public static void main(String[] args) throws Exception {
+        String sMsg1 = "Técnico Informática";
         String sMsg2 = "Semestre";
         int iValor1 = 2; // %d
         int iValor2 = 1; // %d
         float fValor1 = 58.700001f; // %f
-        char cValor = '%'; // c
-        boolean Excelencia = true; // %b
+        char cValor1 = '%'; // %c
+        boolean bExcelência = true; // %b
 
-        // Usando multiplas Strings:
-        // Declarando:
-        String sMsg3 = "Duvidas??? ";
-        //teste
+        String sMsg3 = "Duvidas???";
+        String sMsg4 = "Por favor chamem!!";
+        String sMsg5 = "Muito obrigado pela atenção";
+        String sMsg6 = String.format("%s \n %s.\n \n %s", sMsg3, sMsg4, sMsg5);
 
+        // Saída de dados para o usuário:
+        System.out.println(sMsg6);
     }
 }
