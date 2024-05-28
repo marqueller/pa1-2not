f6e637a46acf1348b17039fc4e4c8298be5b32b7
Jeff013 <600034445@estudante.sed.sc.gov.br>
2024-05-28 20:06:29 -0300

    1° commit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String sMsg1 = "Técnico Informática";
+        String sMsg1 = "Técnico Informática!";
         String sMsg2 = "Semestre";
         int iValor1 = 2; // %d
         int iValor2 = 1; // %d
